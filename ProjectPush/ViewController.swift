7b9dc73ff0b16aade7139dad736e6f56ac99615a
mac <dashamosinaa@yandex.ru>
2020-10-30 13:25:30 +0800

    initial commit

---
+++ ProjectPush/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ProjectPush
+//
+//  Created by mac on 29/10/2020.
+//  Copyright © 2020 kisamosina. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
